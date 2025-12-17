c694d0a5a1010705058486ae37e7187787b61ea3
Luis Aparicio <luis.aparicio4@upr.edu>
2025-12-17 18:27:49 -0400

    Score

--- Assets/Scripts/LifeBar.cs
+++ Assets/Scripts/LifeBar.cs
@@ -1,20 +1,26 @@
 using UnityEngine.UI;
 using UnityEngine;
+using TMPro;
 
 public class LifeBar : MonoBehaviour
 {
    public Image FillLifeBar;
    private Life playerLife;
    private float maxLife;
+   private ScoreManager score;
+   private TextMeshProUGUI scoreText;
    
    void Start()
    {
        playerLife = GameObject.Find("Player").GetComponent<Life>();
        maxLife = playerLife.amount;
+       scoreText =GameObject.Find("Score").GetComponent<TextMeshProUGUI>();
+       score = ScoreManager.instance;
    }
 
    void Update()
     {
-         FillLifeBar.fillAmount = playerLife.amount / maxLife;
+        FillLifeBar.fillAmount = playerLife.amount / maxLife;
+        scoreText.text = "Score: " + score.amount;
     }
 }
